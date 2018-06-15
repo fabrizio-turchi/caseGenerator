eeb1f18699122b1daa83b96aa9f4930508f1758f
Fabrizio <fabrizio.turchi@ittig.cnr.it>
2018-06-15 08:47:11 +0200

    Add relavant comments on Overview form for describing the algorithm used for extracting all data of the Evidence timeline, starting from the Investigative Actions

--- caseGenerator_overview.pas
+++ caseGenerator_overview.pas
@@ -1,5 +1,11 @@
 unit caseGenerator_overview;
-
+{
+  the form display the essential information related to the Evidence timeline. The starting
+  point is the list of Investigative_Action when an item is selected all the correlated data
+  are shown: Who, BRole, Where, When, What, Instrument, Object/Input, Result/Output.
+  The Result are shown in a TreeView component where each node is broken down in Mobule device,
+  SIM, File, Messages, Phone Account, etc.
+}
 interface
 
 uses
@@ -48,6 +54,7 @@ type
     procedure btnAddItemClick(Sender: TObject);
     procedure btnAddChildClick(Sender: TObject);
     procedure tvActionsChange(Sender: TObject);
+    procedure FormShow(Sender: TObject);
   private
     { Private declarations }
   public
@@ -93,6 +100,31 @@ begin
   //addChildren(itemContext, '"rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",' + recSep);
   //addChildren(itemContext, '"rdfs": "http://www.w3.org/2000/01/rdf-schema#",' + recSep);
   //addChildren(itemContext, '"xsd": "http://www.w3.org/2001/XMLSchema#"},' + recSep);
+end;
+
+procedure TformOverview.FormShow(Sender: TObject);
+{
+  1. read investigative_action.JSON for extracting the follwoing data
+    a.  name
+    b.  performer
+    c.  location
+    d.  instrument
+    e.  description
+    f.  object
+    g.  result
+  2.  using 1.b read identity.JSON and role.JSON for setting up the field [Who] and [Role]
+  3.  using 1.c read location.JSON for setting up the field [Where]
+  4.  using 1.d read warrant.JSON, if 1.a=preserved or transferred otherwise read toool.JSON
+      for setting up the field [Instrument]
+  5.  use 1.e for setting up the field [What]
+  6.  using 1.f read trace_XXX.JSON if 1.a=preservec otherwise read provenance_record.JSON
+      for extracting uuid values and use them for reading descriptive info from trace_XXX.JSON
+}
+var
+  listAction: TStringList;
+begin
+  listAction := TStringList.Create;
+
 end;
 
 procedure TformOverview.tvActionsChange(Sender: TObject);
